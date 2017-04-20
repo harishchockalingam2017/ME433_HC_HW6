f797483737e8ac323be4a858f4d5f5dbafafc0cf
harishchockalingam <harishchockalingam2017@u.northwestern.edu>
2017-04-20 15:58:10 -0500

    main.c

---
+++ ME433_HC_HW6.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Harish
+ *
+ * Created on April 20, 2017, 3:56 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
